21eeeadc63332de34c4213ba6b09a5f2262932e7
Sedin-hemakumar <hemakumar@sedintechnologies.com>
2025-12-12 17:06:47 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,10 @@
-output "vpc_id" {
-description = "The ID of the VPC"
-value = aws_vpc.this.id
+output "hosted_zone_name" {
+  value = module.Route53.hosted_zone_name
+}
+output "hosted_zone_id" {
+  value = module.Route53.hosted_zone_id
+}
+output "ZoneType" {
+  description = "PUBLIC or PRIVATE hosted zone"
+  value       = module.Route53.ZoneType
 }
